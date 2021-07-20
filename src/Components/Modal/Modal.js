3768103349ff646c31e65a4ccfa329dc21d55fcf
Tomer Levi <levitomer1996@gmail.com>
2021-07-20 20:59:58 +0300

    fd

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ModalContext from "../../Context/ModalContext";
 import ContentHandler from "./Comps/ContentHandler";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
@@ -9,13 +9,18 @@ import roundClose from "@iconify-icons/ic/round-close";
 import "./Comps/Modal.scss";
 
 export default function GeneralModal() {
-  const { modalState, setIsModalOpen } = useContext(ModalContext);
+  const { modalState, setIsModalOpen, closeModal } = useContext(ModalContext);
   const { isOpen, content, params } = modalState;
   return (
-    <Modal isOpen={isOpen} modalClassName="modal-black">
+    <Modal
+      isOpen={isOpen}
+      modalClassName="modal-black"
+      toggle={setIsModalOpen}
+      scrollable={true}
+    >
       <div className="general_modal">
         <div className={"close-button-div"}>
-          <IconButton onClick={() => setIsModalOpen()}>
+          <IconButton onClick={setIsModalOpen}>
             <Icon
               icon={roundClose}
               style={{ width: 20, height: 20, color: "#823c59" }}
